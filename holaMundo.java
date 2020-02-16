f561ad68275cc3bc2c71db0915153bd31197b3b9
Aldo Gutierrez Arrañaga <aldo@Konoha.Kurama>
2020-02-16 14:53:54 -0600

    Eliminar

--- holaMundo.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-public class holaMundo{
-	public static void main(String[] args) {
-		System.out.println("Hola mundo");
-		JOptionPane.showMessageDialog(null, "Hola mundo 2");
-	}
-}
